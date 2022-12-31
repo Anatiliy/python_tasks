a730e46f1bcfe098bf68191e2194b0ec935c7356
Anatoliy <staheev.a.n@yandex.ru>
2022-12-31 12:51:31 +0600

    дописал функции task_funk и my_filter

--- seminar_3/task3.py
+++ seminar_3/task3.py
@@ -4,8 +4,9 @@
 # Пример:
 
 # [1.1, 1.2, 3.1, 5, 10.01] => 0.19
+# numbers = ['1.1', '1.2', '3.1', '5', '10.01']
 
-
+from decimal import *
 
 
 def check_numbers(numbers):
@@ -21,14 +22,27 @@ def my_map(func, list1):
     return result_list
 
 
+def task_func(numbers):
+    
+    result_list = []
+    for i in numbers:
+        result_list.append(Decimal(i) - (int(float(i))))
+    return result_list    
+
+def my_filter(numbers):
+    result_list = []
+    for i in numbers:
+        if i != 0:
+            result_list.append(i)
+    return result_list
+
 numbers = input('введите вещественные числа через прбел ').split()
 
 while not check_numbers(numbers):
     print('не коректно заданы числа')
     numbers = input('введите вещественные числа через прбел ').split()
 
-#временный вывод для проверки
-print(list(map(float, numbers)))
+print(max(my_filter(task_func(numbers))) - min(my_filter(task_func(numbers))))
 
 
 
