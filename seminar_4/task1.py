84a26acdaa04f64d6c001a9defb9eb9552b87066
Anatoliy <staheev.a.n@yandex.ru>
2023-01-03 13:40:54 +0600

    доработал функцию create_list_coefficients_polynominal и написал функцию create_polynominal

--- seminar_4/task1.py
+++ seminar_4/task1.py
@@ -5,16 +5,37 @@
 
 # если k = 2, то многочлены могут быть => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
 
+
 # функция, создающая список (create_list_coefficients_polynominal) коэффециентов многочлена заданной
 # степени (degree_num) в заданном диапазоне (от begin_item до end_item)
 def create_list_coefficients_polynominal(degree_num, begin_item = 0, end_item = 100):
     import random
-    coefficients_polynominal = sorted(10 * list(range(begin_item, end_item + 1)))
+    coefficients_polynominal = sorted(degree_num * list(range(begin_item, end_item + 1)))
     random.shuffle(coefficients_polynominal)
     coefficients_polynominal = random.sample(coefficients_polynominal, degree_num + 1)
+    if coefficients_polynominal[0] == 0:
+        return create_list_coefficients_polynominal(degree_num, begin_item, end_item)
     return coefficients_polynominal
 
 
+def create_polynominal(degree_num, coefficient_list):
+    result_polynominal = ''
+    for c in coefficient_list:
+        if degree_num > 1 and c != 0:
+            result_polynominal += str(c) + '*x^' + str(degree_num) + ' + '
+            degree_num -= 1
+        elif degree_num == 1 and c != 0:
+            result_polynominal += str(c) + '*x' + ' + '
+            degree_num -= 1
+        elif degree_num == 0 and c != 0:
+            result_polynominal += str(c) + ' = 0'
+    
+    return result_polynominal
+
+
+
 # временный вывод
-print(create_polynominal(int(input('введите натуральное число '))))    
+
+degree_num = int(input('введите натуральное число '))
+print(create_polynominal(degree_num, create_list_coefficients_polynominal(degree_num)))  
 
