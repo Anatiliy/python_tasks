30a9cccf5b5133d64c8fd8a82f99abace2172f2f
Anatoliy <staheev.a.n@yandex.ru>
2023-01-06 11:22:42 +0600

    написал функцию extract_from_file, внёс изменения в существующие функци, написал основную программу, написал коментарии

--- seminar_4/task1.py
+++ seminar_4/task1.py
@@ -15,8 +15,9 @@
 
 
 
-# функция, создающая список (create_list_coefficients_polynominal) коэффециентов многочлена заданной
-# степени (degree_num) в заданном диапазоне (от begin_item до end_item)
+# функция, создающая список коэффециентов многочлена "create_list_coefficients_polynominal"
+# заданной степени "degree_num" в заданном диапазоне от "begin_item" до "end_item"
+
 def create_list_coefficients_polynominal(degree_num, begin_item = 0, end_item = 100):
     import random
     coefficients_polynominal = sorted(degree_num * list(range(begin_item, end_item + 1)))
@@ -29,10 +30,10 @@ def create_list_coefficients_polynominal(degree_num, begin_item = 0, end_item = 
         return coefficients_polynominal
 
 
-# функция, создающая многочлен степени degree_num с коэффициентами coefficient_list,
-# записанный в виде строки
+# функция, создающая многочлен строкового типа "result_polynominal" из списка
+# коэффициентов "coefficient_list"
 
-def create_polynominal(degree_num, coefficient_list):
+def create_polynominal(coefficient_list):
     result_polynominal = ''
     for key, value in coefficient_list.items():
         result_polynominal += str(value) + '*x^' + str(key) + ' + '
@@ -41,10 +42,15 @@ def create_polynominal(degree_num, coefficient_list):
     return result_polynominal
 
 
+# функция, записывающая заданный текст "text_of_entry" в файл с заданным именем "file_name"
+
 def writing_to_file(text_of_entry, file_name):
     with open(file_name, 'w', encoding='utf-8') as output_file:
         output_file.write(text_of_entry)
 
+
+# функция, извлекающая список коэффициентов "coefficients_polynominal" из заданного
+# многочлена строкового типа "polynominal"
 
 def separation_of_coefficients_of_polynomial(polynominal):
     polynominal = polynominal.replace('- ', '+ -').rstrip(' = 0').split(' + ')
@@ -63,32 +69,44 @@ def separation_of_coefficients_of_polynomial(polynominal):
     return coefficients_polynominal
 
 
+# функция, извлекающая строку "coefficients_polynominal" из файла заданного имени "file_name"
+
+def extract_from_file(file_name):
+    with open(file_name, 'r', encoding='utf-8') as input_file:
+        coefficients_polynominal = input_file.readline()
+    return coefficients_polynominal    
 
 
-def addition_of_polynominals(file1_name, file2_name):
-    with open(file1_name, 'r', encoding='utf-8') as input_file1, open(file2_name, 'r', encoding='utf-8') as input_file2:
-        coefficients_polynominal1 = separation_of_coefficients_of_polynomial(input_file1.readline())
-        coefficients_polynominal2 = separation_of_coefficients_of_polynomial(input_file2.readline())
+# функция, создающая список коэффициентов многочлена "result_coefficients_polynominal" из
+# заданных списков коэффициентов многочлена "coefficients_polynominal1" и
+# "coefficients_polynominal2", путём сложения коэффициентов одноимённых степеней
 
+def addition_of_polynominals(coefficients_polynominal1, coefficients_polynominal2):
     if max(coefficients_polynominal1) > max(coefficients_polynominal2):
         result_coefficients_polynominal = {}
         for key in  range(max(coefficients_polynominal1), -1, -1):
             result_coefficients_polynominal[key] = coefficients_polynominal1.get(key, 0) + coefficients_polynominal2.get(key, 0)
     else:
         result_coefficients_polynominal = {}
-        for key in range(max(coefficients_polynominal1), -1, -1):
+        for key in range(max(coefficients_polynominal2), -1, -1):
             result_coefficients_polynominal[key] = coefficients_polynominal1.get(key, 0) + coefficients_polynominal2.get(key, 0)
 
     return result_coefficients_polynominal
 
     
 
-# временный вывод
+# основная программа
 
-#degree_num = int(input('введите натуральное число '))
+for _ in range(int(input('введите количество создаваемых файлов '))):
+    degree_num = int(input('задайте степень многочлена в виде натурального числа '))
+    begin_item = int(input('задайте начало диапазона многочленов в виде натурального числа '))
+    end_item = int(input('задайте конец диапазона многочленов в виде натурального числа '))
 
-#polynominal = create_polynominal(degree_num, create_list_coefficients_polynominal(degree_num, begin_item = -100))
-#print(polynominal)
-#print(separation_of_coefficients_of_polynomial(polynominal))
-#writing_to_file(polynominal, input('введите имя файла, используя латинские буквы и цифры ') + '.txt')
-addition_of_polynominals(input('введите имя файла 1, используя латинские буквы и цифры ') + '.txt',input('введите имя файла 2, используя латинские буквы и цифры ') + '.txt')
+    polynominal = create_polynominal(create_list_coefficients_polynominal(degree_num, begin_item, end_item))
+    print(polynominal)
+    writing_to_file(polynominal, input('введите имя файла, используя латинские буквы и цифры ') + '.txt')
+
+
+polynominal = create_polynominal(addition_of_polynominals(separation_of_coefficients_of_polynomial(extract_from_file(input('введите имя файла 1, используя латинские буквы и цифры ') + '.txt')), separation_of_coefficients_of_polynomial(extract_from_file(input('введите имя файла 2, используя латинские буквы и цифры ') + '.txt'))))
+print(polynominal)
+writing_to_file(polynominal, input('введите имя файла, используя латинские буквы и цифры ') + '.txt')
