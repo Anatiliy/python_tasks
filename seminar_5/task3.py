78d572b83992ecc563a883d3eac78fcce510b35b
Anatoliy <staheev.a.n@yandex.ru>
2023-01-21 18:35:51 +0600

    написал функцию main_program, изменил функции file_compression и file_expantion, написал основную программу

--- seminar_5/task3.py
+++ seminar_5/task3.py
@@ -15,26 +15,40 @@ def writing_to_file(text_of_entry, file_name):
         output_file.write(text_of_entry)
 
 
-def file_compression(original_data):
-    intermediate_result = list([])
+def file_compression(source_file, result_file):
+    source_data = extract_from_file(source_file)
+    intermediate_result = [[0, source_data[0]]]
     index = 0
-    for char in original_data:
+    for char in source_data:
         if char in intermediate_result[index]:
             intermediate_result[index][0] += 1
         else:
-            intermediate_result[index] = [1, char]
-            intermediate_result.append([])
             index += 1
-    result = ''.join(tuple(map(lambda item: str(item[0]) + item[1]), intermediate_result))
-    return result
+            intermediate_result.append([1, char])
+    result = ''.join(tuple(map(lambda item: str(item[0]) + item[1], intermediate_result)))
+    writing_to_file(result, result_file)
+    
 
-
-def file_expantion(compressed_file):
-    intermediate_result = str(map(lambda char: char + ' ' if char.isalpha() else char, compressed_file)).split()
+def file_expantion(source_file, result_file):
+    compressed_data = extract_from_file(source_file)
+    intermediate_result = ''.join(map(lambda char: char + ' ' if not char.isdigit() else char, compressed_data)).split()
     result = ''.join(map(lambda item: item[-1] * int(item[:-1]), intermediate_result))
-    return result
+    writing_to_file(result, result_file)
 
 
- 
+def main_program():
+    key = input('введите "С", если нужно сжать файл, введите "P", если нужно расжать файл ')
+    if key in 'cCсС':
+        file_compression(input('введите имя сжимаемого файла ') + '.txt', input('введите имя файла для хранения сжатых данных ') + '.txt')
+    elif key in 'PpРр':
+        file_expantion(input('введите имя расжимаемого файла ') + '.txt', input('введите имя файла для хранения расжатых данных ') + '.txt')
+    else:
+        print('ввод не верный')
+        return main_program()
 
 
+#основная программа
+data = 'dddddkjfkja;;;ddddkkkkkkkjjgaeeeeioiamdnnnnnnnnnnnnnnlalalakddddddddddddddd'
+writing_to_file(data, 'f1.txt')
+
+main_program()
