bd43e9d60c9479682c2d25ae8d572360c8fbe7dc
Anatoliy <staheev.a.n@yandex.ru>
2023-01-23 16:54:00 +0600

    написал функцию calculating_expression, функции математических операций и основную программу

--- seminar_6/seminar_task.py
+++ seminar_6/seminar_task.py
@@ -1,19 +1,44 @@
 
 
 
+def adding(a, b):
+    return a + b
+
+
+def subtracting(a, b):
+    return a - b
+
+
+def multiplication(a, b):
+    return a * b
+
+
+def dividing(a, b):
+    return a / b
+
+
+def exponentiation(a, b):
+    return a ** b
+
 def calculating_expression(expression):
+    math_operations = {'^': exponentiation, '*': multiplication, '/': dividing, '+': adding, '-': subtracting}
     if '(' in expression:
         begin_index = expression.index('(') + 1
         end_index = len(expression) - expression[::-1].index(')') - 1
-        expression = expression[:begin_index - 1] + calculating_expression(expression[begin_index:end_index]) + expression[end_index + 1:]
+        expression = expression[:begin_index - 1] + [calculating_expression(expression[begin_index:end_index])] + expression[end_index + 1:]
+    for key in math_operations:
+        while key in expression:
+            index = expression.index(key)
+            a = expression[index - 1]
+            b = expression[index + 1]
+            expression = expression[:index - 1] + [math_operations[key](a, b)] + expression[index + 2:]
+    return expression[0]
 
 
 
-st = '(26 + 9 * 2 - 4 * (5 - 7)) / 3'
-lst = ''.join(map(lambda item: ' ' + item + ' ' if not item.isdigit() else item, st)).split()
+
+st = '(26 + 9 ** 2 - 4 * (5 - 7)) : 3'
+st = st.replace('**', '^').replace(':', '/')
+lst = list(map(lambda item: int(item) if item.isdigit() else item, ''.join(map(lambda item: ' ' + item + ' ' if not item.isdigit() else item, st)).split()))
 print(lst)
-print(lst.index('('), len(lst) - lst[::-1].index(')') - 1)
-b = lst.index('(') + 1
-e = len(lst) - lst[::-1].index(')') - 1
-print(lst[b:e])
-print(sum((b, -e)))
+print(calculating_expression(lst))
