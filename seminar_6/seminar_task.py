6051354081ea1978a474a4d3b0d3660fdd415601
Anatoliy <staheev.a.n@yandex.ru>
2023-01-24 11:42:26 +0600

    изменил функцию calculating_expression, дописал основную программу, добавил коментарии

--- seminar_6/seminar_task.py
+++ seminar_6/seminar_task.py
@@ -1,4 +1,4 @@
-
+# программа для вычисления значения математического выражения
 
 
 def adding(a, b):
@@ -20,8 +20,15 @@ def dividing(a, b):
 def exponentiation(a, b):
     return a ** b
 
+
+# функция вычисления значения выражения "expression"
 def calculating_expression(expression):
     math_operations = {'^': exponentiation, '*': multiplication, '/': dividing, '+': adding, '-': subtracting}
+    # проверка на отрицательное число в начале выражения
+    if expression[0] == '-':
+        expression[1] *= -1
+        expression.pop(0)
+    # выделение выражения в скобках, для последующего вычисления
     if '(' in expression:
         begin_index = expression.index('(') + 1
         end_index = len(expression) - expression[::-1].index(')') - 1
@@ -36,8 +43,9 @@ def calculating_expression(expression):
 
 
 
-
-st = '(26 + 9 ** 2 - 4 * (5 - 7)) : 3'
+# основная программа
+#st = '(-26 + 9 ** 2 - 4 * (-5 - 7)) : 3'
+st = input('введите выражение для вычисления ')
 st = st.replace('**', '^').replace(':', '/')
 lst = list(map(lambda item: int(item) if item.isdigit() else item, ''.join(map(lambda item: ' ' + item + ' ' if not item.isdigit() else item, st)).split()))
 print(lst)
