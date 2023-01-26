7b9193268fc7768209f3bc4284884bd7f5a7df58
Anatoliy <staheev.a.n@yandex.ru>
2023-01-26 12:03:22 +0600

    написал функцию delete, написал коментарии

--- seminar_7/controller.py
+++ seminar_7/controller.py
@@ -1,13 +1,22 @@
 import data_processing as dp
 import view as vw
 
+
+# вывод справочника на экран
 def show_directory():
     vw.show(dp.browse())
 
 
+# вывод найденого контакта на экран
 def show_search_result():
     vw.show(dp.find())
 
 
+# запись контакта в справочник
 def write_data():
     dp.writing()
+
+
+# удаление контакта из справочника
+def delete_data():
+    dp.delete()
