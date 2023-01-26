2de63e6f363924cd7417658d8d54b2daab81a0ce
Anatoliy <staheev.a.n@yandex.ru>
2023-01-26 11:56:30 +0600

    написал функцию delete, написал коментарии

--- seminar_7/data_processing.py
+++ seminar_7/data_processing.py
@@ -1,16 +1,19 @@
 
 
+# запись информации в файл
 def writing():
     with open('telephone_directory.csv', 'a', encoding='utf-8') as output_file:
         print(input('введите фамилию '), input('введите имя '), input('введите номер телефона '), sep=';', file=output_file)
 
 
+# выгрузка информации из файла
 def browse():
     with open('telephone_directory.csv', 'r', encoding='utf-8') as input_file:
         data = list(map(lambda item: item.strip().split(';'), input_file.readlines()))
     return data
 
 
+# поиск информации в файле по ключу "key"
 def find():
     key = input('введите фамилию, имя или номер телефона ')
     print()
@@ -24,5 +27,23 @@ def find():
             data = input_file.readline()
     if result:
         return result
+    else:
+        print('контакт не найден')
+
+
+# удаление информации из файла по ключу "key"
+def delete():
+    key = input('введите номер телефона ')
+    print()
+    with open('telephone_directory.csv', 'r', encoding='utf-8') as input_file:
+        data = list(map(lambda item: item.strip().split(';'), input_file.readlines()))
+    result = []
+    for item in data:
+        if key in item:
+            data.pop(data.index(item))
+            print('контакт удалён')
+            with open('telephone_directory.csv', 'w', encoding='utf-8') as output_file:
+                output_file.writelines(list(map(lambda item: ';'.join(item) + '\n', data)))
+            break
     else:
         print('контакт не найден')
