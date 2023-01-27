b188e56ab83172cb23879d80b45137c0dd62b8a6
Anatoliy <staheev.a.n@yandex.ru>
2023-01-27 10:49:30 +0600

    внёс глобальные изменения в модуль

--- seminar_7/data_processing.py
+++ seminar_7/data_processing.py
@@ -1,49 +1,33 @@
 
 
-# запись информации в файл
-def writing():
+# запись информации из переменной "data" в файл
+def write_line(data):
     with open('telephone_directory.csv', 'a', encoding='utf-8') as output_file:
-        print(input('введите фамилию '), input('введите имя '), input('введите номер телефона '), sep=';', file=output_file)
+        print(*data, sep=';', file=output_file)
 
 
-# выгрузка информации из файла
-def browse():
+# считывание всей информации из файла в переменную "data"
+def read_file():
     with open('telephone_directory.csv', 'r', encoding='utf-8') as input_file:
         data = list(map(lambda item: item.strip().split(';'), input_file.readlines()))
     return data
 
 
-# поиск информации в файле по ключу "key"
-def find():
-    key = input('введите фамилию, имя или номер телефона ')
-    print()
+# перезапись файла информацией из переменной "data"
+def overwrite(data):
+    with open('telephone_directory.csv', 'w', encoding='utf-8') as output_file:
+        output_file.writelines(list(map(lambda item: ';'.join(item) + '\n', data)))
+
+
+# поиск информации в файле по ключу "key" с последующей записью в переменную "result_data"
+def find(key):
     with open('telephone_directory.csv', 'r', encoding='utf-8') as input_file:
         data = input_file.readline()
-        result = []
+        result_data = []
         while data != '':
             data = data.strip().split(';')
             if key.lower() in list(map(lambda item: item.lower(), data)):
-                result.append(data)
+                result_data.append(data)
             data = input_file.readline()
-    if result:
-        return result
-    else:
-        print('контакт не найден')
-
-
-# удаление информации из файла по ключу "key"
-def delete():
-    key = input('введите номер телефона ')
-    print()
-    with open('telephone_directory.csv', 'r', encoding='utf-8') as input_file:
-        data = list(map(lambda item: item.strip().split(';'), input_file.readlines()))
-    result = []
-    for item in data:
-        if key in item:
-            data.pop(data.index(item))
-            print('контакт удалён')
-            with open('telephone_directory.csv', 'w', encoding='utf-8') as output_file:
-                output_file.writelines(list(map(lambda item: ';'.join(item) + '\n', data)))
-            break
-    else:
-        print('контакт не найден')
+    return result_data
+    
